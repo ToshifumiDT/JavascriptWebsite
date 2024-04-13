08bc080baac488c8012560df1132ba64e32fb76e
ToshifumiDT <toshifumi.digitaletalenter@gmail.com>
2024-04-13 19:48:53 +0200

    button

---
+++ index.js
@@ -0,0 +1,8 @@
+document.getElementById("Button").addEventListener("click", function() {
+    window.open("./page2.html", "_blank");
+  });
+
+
+document.getElementById("Button2").addEventListener("click", function() {
+    window.open("https://github.com/ToshifumiDT/selfsite.git", "_blank");
+  });
